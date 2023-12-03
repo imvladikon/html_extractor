deff2d068f6c7f609fe51e35de2b7402365627be
Vladimir Gurevich <imvladikon@gmail.com>
2023-12-03 22:00:16 +0200

    added setup

--- data_extractor/__init__.py
+++ data_extractor/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from data_extractor.version import __version__
 from data_extractor.data_transformers import ExtractiveHtml2JsonTransformer
 from data_extractor.data_transformers import RuleBasedHtml2JsonTransformer
 from data_extractor.data_schemes import TableSchema
